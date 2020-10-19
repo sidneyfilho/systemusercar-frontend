7cb2217d4f220c7b0c9608f788b043553c6cc4fe
SidneyCFilho <sidpfilho@gmail.com>
2020-10-19 01:07:31 -0300

    Ajuste no axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const service = axios.create({
     baseURL: 'https://cors-anywhere.herokuapp.com/https://desafio-pitang-java.herokuapp.com/api',
     headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:8080',
+        'Access-Control-Allow-Origin': 'https://desafio-pitang-frontend.herokuapp.com:8080',
         'Access-Control-Allow-Headers': 'POST, GET, OPTIONS, DELETE',
     }
 });
@@ -14,10 +14,9 @@ service.interceptors.request.use(async (request) => {
 });
 
 service.interceptors.response.use(function (response) {
-    if (response.data.status == 'Token is Expired') {
-        localStorage.removeItem('token')
-        Vue.prototype.$notification.show('SESSION_EXPIRED')
-    }
+    // if (response.data.status == 'Token is Expired') {
+        // localStorage.removeItem('token')
+    // }
     return response
 });
 
